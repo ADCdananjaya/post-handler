7d9cece5582f502e5aac11a6d9e1400f2af46053
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-05-19 14:09:12 +0530

    Add post and put routes to backend.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,45 @@
-const express = require('express');
+const express = require("express");
 const posts = require("./data.js");
 const cors = require("cors");
 const app = express();
 const port = 3000;
 
 app.use(cors());
-app.get('/api/posts', (req, res) => res.json(posts))
+app.use(express.json());
 
-app.listen(port, () => console.log(`http-backend listening on port ${port}!`))
+app.get("/api/posts", (req, res) => res.json(posts));
+
+app.delete("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const index = posts.findIndex((post) => post.id == id);
+  if (index !== -1) {
+    const deletedPost = posts.splice(index, 1);
+    res.status(200).json(deletedPost);
+  } else res.status(404).json({ error: "Post not found!" });
+});
+
+app.post("/api/posts", (req, res) => {
+  const { title, id } = req.body;
+  const post = {
+    userId: 0,
+    id,
+    title,
+    body: "",
+  };
+
+  posts.push(post);
+  res.status(200).json(post);
+});
+
+app.put("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const { title } = req.body;
+
+  const updatedPost = posts.find((post) => post.id == id);
+  if (updatedPost) {
+    updatedPost.title = title;
+    res.status(200).json(updatedPost);
+  } else res.status(404).json({ Error: "Post not found!" });
+});
+
+app.listen(port, () => console.log(`http-backend listening on port ${port}!`));
